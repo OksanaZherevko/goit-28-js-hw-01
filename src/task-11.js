f4791d19e9213993b07edd7069d171edacce8134
Oksana Zherevko <squirrelksi@ukr.net>
2021-02-10 17:50:53 +0200

    Add task-11.

--- src/task-11.js
+++ src/task-11.js
@@ -1,6 +1,16 @@
+function calculateTotalPrice (orderedQuantity, pricePerItem) {
+  // Пиши код ниже этой строки
+  const totalPrice = orderedQuantity*pricePerItem;
 
+  // Пиши код выше этой строки
+  return totalPrice;
+};
 
-
+// console.log(calculateTotalPrice(5, 100));
+// console.log(calculateTotalPrice(8, 60));
+// console.log(calculateTotalPrice(3, 400));
+// console.log(calculateTotalPrice(1, 3500));
+// console.log(calculateTotalPrice(12, 70));
 
 // Задание
 // Функция calculateTotalPrice считает и возвращает общую сумму покупки. Она принимает два параметра, значения которых будут задаваться во время её вызова.
